f9383c116d1e6bd2b1e675490289f5af24e46ddb
Mason Inman <76747556+MasonInman29@users.noreply.github.com>
2022-05-23 09:07:57 -0500

    Delete PhoneBook Project (Class Inheritance) directory

--- PhoneBook Project (Class Inheritance)/RPG.py
+++
@@ -1,9 +0,0 @@
-from Chacacter import *
-
-class RPG(Character):
-  def __init__(self, name, type, benchMax, role):
-    super().__init__(name, type, benchMax)
-    self.role = role
-    
-  def __str__(self):
-    return (self.type + ' ' + self.role + ' ' + self.name)
